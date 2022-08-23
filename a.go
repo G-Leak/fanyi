623de62befe4ddba24d18f83210faf97ef8aff8d
Your Name <you@example.com>
2022-08-23 13:03:07 +0800

    demo

--- a.go
+++ a.go
@@ -4,6 +4,13 @@ import "fmt"
 
 func main() {
 	a := 13
-	fmt.Println(a)
+	b := 23
+
+	c := compare(a, b)
+	fmt.Println(c)
 	fmt.Println("hello world!")
+}
+
+func compare(a int, b int) int {
+	return a + b
 }
