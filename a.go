236477ae71d06dfa8f7a53263c9846a317303eaa
Your Name <you@example.com>
2022-08-24 22:09:24 +0800

    demo

--- a.go
+++ a.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gonum.org/v1/gonum/stat"
+)
 
 func main() {
 	a := 13
@@ -9,6 +12,10 @@ func main() {
 	c := compare(a, b)
 	fmt.Println(c)
 	fmt.Println("hello world!")
+
+	arr := []float64{1, 2, 5, 6}
+	v := stat.Variance(arr, nil)
+	fmt.Printf("方差=%f", v)
 }
 
 func compare(a int, b int) int {
